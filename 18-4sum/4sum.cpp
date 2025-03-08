c78196ce5a34e4ff64015c94c21ea5e6c079e9af
Thakur Prasad Rout <78676104+thakurtpr@users.noreply.github.com>
2025-03-09 00:35:21 +0530

    Time: 25 ms (36.65%) | Memory: 17.4 MB (59.86%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -8,21 +8,20 @@ public:
             for(int j = i+1; j< nums.size();){
                 int p = j+1,q=nums.size()-1;
                 while(p<q){
-                    long long sum = (long long )nums[i]+(long long )nums[j]+(long long )nums[p]     +(long long )nums[q];
+                    long long sum = (long long )nums[i]+(long long )nums[j]+(long long )nums[p]+(long long )nums[q];
                     if(sum<target){
                         p++;
                     }else if(sum>target){
                         q--;
                     }else{
                         result.push_back({nums[i],nums[j],nums[p],nums[q]});
-
                         p++;q--;
-
                         while(p<q && nums[p]==nums[p-1]) p++;
                     }
                 }
                 j++;
                 while(j<nums.size() && nums[j]==nums[j-1]) j++;
+                
             }
         }
         return result;
