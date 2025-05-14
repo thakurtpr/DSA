6352bbc773aa5cb6723d30c827b2924fbed04d53
Thakur Prasad Rout <78676104+thakurtpr@users.noreply.github.com>
2025-05-14 11:22:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (27.81%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        vector<int>temp;
-        int r=k,count=1;
+        // vector<int>temp;
+        // int r=k,count=1;
 
-        for(int i = 0 ;i<arr.size();i++,count++){
-            if(count==arr[i]){
-                // cout<<"count inside if="<<count<<endl;
-                continue;
-            }
-            while(count!=arr[i]){
-                // cout<<"count inside while "<<count<<endl;
-                if(k==0) break;
-                temp.push_back(count);
-                count++;
-                k--;
-                cout<<k<<endl;
-            }
+        // for(int i = 0 ;i<arr.size();i++,count++){
+        //     if(count==arr[i]){
+        //         // cout<<"count inside if="<<count<<endl;
+        //         continue;
+        //     }
+        //     while(count!=arr[i]){
+        //         // cout<<"count inside while "<<count<<endl;
+        //         if(k==0) break;
+        //         temp.push_back(count);
+        //         count++;
+        //         k--;
+        //         cout<<k<<endl;
+        //     }
 
+        // }
+        // if(k!=0){
+        //     return arr[arr.size()-1]+k;
+        // }
+        // // cout<<"temp "<<temp[r];
+        // return temp[r-1];
+        int low = 0, n = arr.size(),high = n-1;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            int value = arr[mid] - (mid+1);
+            if(value<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
         }
-        if(k!=0){
-            return arr[arr.size()-1]+k;
-        }
-        // cout<<"temp "<<temp[r];
-        return temp[r-1];
+        return low+k;
     }
 };
