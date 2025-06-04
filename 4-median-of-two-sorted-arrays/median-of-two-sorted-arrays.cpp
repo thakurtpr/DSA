be39f554ba9b78710831c2299d0a33aeefc77504
Thakur Prasad Rout <78676104+thakurtpr@users.noreply.github.com>
2025-06-04 12:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.65%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -7,7 +7,7 @@ public:
         int low= 0 ,high = n1;
         int left = (n1+n2+1)/2;
         int n  = n1+n2;
-        cout<<"n is "<<n<<endl;
+        // cout<<"n is "<<n<<endl;
         // int mid1=()
         while(low<=high){
             int mid1=low+(high-low)/2;
@@ -20,9 +20,9 @@ public:
             if(mid2<n2) r2=nums2[mid2];
             if(mid1-1>=0) l1=nums1[mid1-1];
             if(mid2-1>=0) l2=nums2[mid2-1];
-            cout<<"l1="<<l1<<"l2="<<l2<<"r1="<<r1<<"R2="<<r2<<endl;
+            // cout<<"l1="<<l1<<"l2="<<l2<<"r1="<<r1<<"R2="<<r2<<endl;
             if(l1<=r2 && l2<=r1){
-                cout<<"max"<<max(l1,l2);
+                // cout<<"max"<<max(l1,l2);
                 if(n%2==1) return max(l1,l2);
                 return (double)((max(l1,l2)+min(r1,r2))/2.0);
             }else if(l1>r2) high=mid1-1;
